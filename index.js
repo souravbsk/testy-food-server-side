8d92d70a86206e94b4f2bd72906649def653b290
souravbsk <souravbsk01@gmail.com>
2023-05-02 00:59:15 +0600

    chefs data get to show

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const app = express();
 var cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const chefs = require('./datas/chefs.json');
+
 app.use(cors())
 
 
 app.get("/",(req,res) => {
     res.send("hello world this is testy food server")
 })
+
+app.get("/chefs",(req,res) => {
+    res.send(chefs)
+})
+
 
 
 app.listen(port,() => {
